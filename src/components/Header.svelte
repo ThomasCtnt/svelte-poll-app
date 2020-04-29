58e815c0c26ec1fdc71bd0a835b25846cfed3b33
Thomas Coutant <thomas.coutant@outlook.fr>
2020-04-29 12:49:09 +0200

    Creates a header and a footer component

---
+++ src/components/Header.svelte
@@ -0,0 +1,15 @@
+<header>
+    <h1>Svelte Poll App</h1>
+</header>
+
+<style>
+    header {
+        background-color: #f7f7f7;
+        padding: 20px;
+    }
+
+    h1 {
+        margin: 0;
+        text-align: center;
+    }
+</style>
