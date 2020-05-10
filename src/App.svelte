4d78280a750cbde9bec8b4cf1e0f22c1cbd7e6bb
Thomas Coutant <thomas.coutant@outlook.fr>
2020-05-10 22:06:23 +0200

    Creates a tab component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,20 @@
 <script>
 	import Header from './components/Header.svelte';
 	import Footer from './components/Footer.svelte';
+	import Tabs from './shared/Tabs.svelte';
+
+	// Tabs
+	let items = ['Current Polls', 'Add New Poll'];
+	let activeItem = 'Current Polls';
+
+	const tabSetActive = (e) => {
+		activeItem = e.detail;
+	}
 </script>
 
 <Header />
 <main>
-
+	<Tabs {items} {activeItem} on:tabChange={tabSetActive} />
 </main>
 <Footer />
 
